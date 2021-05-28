13d20d051353b87d55ca0ff3759767d0b7b464e6
tangchen <tc704321764@outlook.com>
2021-05-28 14:20:31 +0800

    提交文件接口

--- sweet-home/src/main/java/com/gitdatsanvich/sweethome/util/ThumbnailUtil.java
+++ sweet-home/src/main/java/com/gitdatsanvich/sweethome/util/ThumbnailUtil.java
@@ -16,7 +16,6 @@ import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
-import java.util.UUID;
 import java.util.concurrent.atomic.AtomicInteger;
 
 /**
