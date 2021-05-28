e6b1c3459151d8aec99928238a6c9bd32c450af2
tangchen <tc704321764@outlook.com>
2021-05-28 14:21:05 +0800

    提交文件接口

--- sweet-home/src/main/java/com/gitdatsanvich/sweethome/model/dto/FileResponseDTO.java
+++ sweet-home/src/main/java/com/gitdatsanvich/sweethome/model/dto/FileResponseDTO.java
@@ -1,9 +1,12 @@
 package com.gitdatsanvich.sweethome.model.dto;
 
+import lombok.Data;
+
 /**
  * @author TangChen
  * @date 2021/5/28 13:56
  */
+@Data
 public class FileResponseDTO {
     private String url;
     private String fileType;
